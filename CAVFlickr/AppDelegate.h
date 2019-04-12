1e3b260cd5e303c61d0a77ffba4119add8a51c12
Александр <achernov@aplana.com>
2019-04-12 22:02:13 +0300

    Initial Commit

---
+++ CAVFlickr/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CAVFlickr
+//
+//  Created by Александр on 12/04/2019.
+//  Copyright © 2019 Александр. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ CAVFlickr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CAVFlickr
+//
+//  Created by Александр on 12/04/2019.
+//  Copyright © 2019 Александр. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
